967ef49ed82ad88a620cdd77ad2fb7ddaf7f893a
Maqatur <maqaturs@gmail.com>
2024-02-14 22:07:33 +0400

    Parameterized Test

--- src/test/java/ru/netology/services/homeworkmvn3/RestServiceTest.java
+++ src/test/java/ru/netology/services/homeworkmvn3/RestServiceTest.java
@@ -2,27 +2,19 @@ package ru.netology.services.homeworkmvn3;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestServiceTest {
-    @Test
-    public void testForaPoolFreelancer() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/rest.csv")
+    public void testForaFreelancer(int expected, int income, int expenses, int threshold) {
 
         RestService service = new RestService();
+        int actual = service.calcRest(income, expenses, threshold);
 
-        int expected = 3;
-        int actual = service.calcRest(10_000, 3_000, 20_000);
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Test
-    public void testForaRichFreelanser() {
-
-        RestService service = new RestService();
-
-        int expected = 2;
-        int actual = service.calcRest(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
 }
